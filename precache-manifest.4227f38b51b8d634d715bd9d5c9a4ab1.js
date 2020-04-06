c77f51354cd7e4f2b3d8c0bc71885688ae9774a6
GH <ginohagad@gmail.com>
2020-04-06 19:00:28 +1200

    Updates

---
+++ precache-manifest.4227f38b51b8d634d715bd9d5c9a4ab1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1acca9d0d89583e40bf219c66130c26e",
+    "url": "/cnci_pdfreader/index.html"
+  },
+  {
+    "revision": "8e356036b8de4cc9f7bf",
+    "url": "/cnci_pdfreader/static/css/main.b0557a1c.chunk.css"
+  },
+  {
+    "revision": "5a5213cf713b45f68537",
+    "url": "/cnci_pdfreader/static/js/2.76cefe70.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cnci_pdfreader/static/js/2.76cefe70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e356036b8de4cc9f7bf",
+    "url": "/cnci_pdfreader/static/js/main.27b14f55.chunk.js"
+  },
+  {
+    "revision": "4a8be7dac0f2efa11268",
+    "url": "/cnci_pdfreader/static/js/runtime-main.5d829d34.js"
+  }
+]);
